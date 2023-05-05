51969e7059329f9fd3a9524831b417b209cb34ed
hazembensaid07 <58050302+hazembensaid07@users.noreply.github.com>
2023-05-05 08:39:52 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,32 @@ import gantt from "dhtmlx-gantt";
 
 const App = () => {
   useEffect(() => {
-    gantt.config.xml_date = null;
-    gantt.config.work_time = {
-      day: [true, true, true, true, true, false, false],
-      hours: {
-        from: 32400,
-        to: 61200,
-      },
+    console.log("here");
+    const tasks = {
+      data: [
+        { id: 1, text: "buy_arm arm1", start_date: new Date(), duration: 2 },
+      ],
+    };
+
+    gantt.date.seconds_start = function (date) {
+      return date.getTime() * 1000;
     };
 
     gantt.config.date_scale = "%s";
-    gantt.config.task_date = "%s";
-    gantt.config.scale_height = 60;
-    gantt.config.drag_links = false;
-    gantt.config.drag_progress = false;
-    gantt.config.grid_width = 420;
+    gantt.init("gantt_here");
+    gantt.parse(tasks);
 
-    gantt.init("gantt_chart");
-
-    gantt.parse([
-      { id: 1, text: "buy_arm arm1", start_date: 0.0, duration: 5000.0 },
-    ]);
+    const task = gantt.getTaskByIndex(0);
+    const endDate = gantt.calculateEndDate(
+      task.start_date,
+      task.duration * 1000
+    );
+    gantt.config.max_date = endDate;
+    gantt.config.min_date = task.start_date;
+    gantt.render();
   }, []);
 
-  return <div id="gantt_chart" style={{ width: "100%", height: "100%" }}></div>;
+  return <div id="gantt_here" style={{ width: "100%", height: "100%" }}></div>;
 };
 
 export default App;
